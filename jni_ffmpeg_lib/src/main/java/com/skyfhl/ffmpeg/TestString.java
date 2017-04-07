d7bd09dbcdd972567d22b65e53f89e60fc2fa49f
skyfhl@hotmail.com <skyfhl@hotmail.com>
2017-04-07 17:29:34 +0800

    first

---
+++ jni_ffmpeg_lib/src/main/java/com/skyfhl/ffmpeg/TestString.java
@@ -0,0 +1,11 @@
+package com.skyfhl.ffmpeg;
+
+/**
+ * Created by hilo on 2017/4/7.
+ */
+
+public class TestString {
+    public static String getString(){
+        return "just test String";
+    }
+}
